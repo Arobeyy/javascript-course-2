d59f67740ed69659529c805860746d73d39b218b
arobey <sumiyaakter.arobey@gmail.com>
2024-07-27 20:22:40 +0600

    done button updated

--- scripts/my-todo-list.js
+++ scripts/my-todo-list.js
@@ -8,11 +8,13 @@ function renderTodoList() {
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
     const html = `
-        <div>&bull; ${todoObject.name} </div>
-        <div>due: ${todoObject.dueDate} </div>
-        <button id = "doneButton" onclick = "
-          addClassToButton();
-          "class = "done-todo-button">
+        <div id = "js-input-text-${i}" >&bull; ${todoObject.name} 
+        </div>
+        <div id = "js-date-text-${i}">due: ${todoObject.dueDate} 
+        </div>
+        <button onclick = "
+          addClassToText(${i});
+          " class = "done-todo-button">
           <img class = "done-icon" src="icons/check-green.png">
         </button>
         <button onclick = "
@@ -24,37 +26,38 @@ function renderTodoList() {
     `;
     todoListHtml += html;
   }
-  document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+  document.querySelector(".js-todo-list").innerHTML = todoListHtml;
 }
 
-function addClassToButton() {
-  const button = document.getElementById('doneButton');
-  button.classList.add('clicked');
+function addClassToText(index) {
+  const inputText = document.getElementById(`js-input-text-${index}`);
+  inputText.classList.add("done-clicked");
+  const dateText = document.getElementById(`js-date-text-${index}`);
+  dateText.classList.add("done-clicked");
 }
 
 function addTodo() {
-  const nameInputElement = document.querySelector('.js-name-input');
+  const nameInputElement = document.querySelector(".js-name-input");
   const name = nameInputElement.value;
 
-  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dateInputElement = document.querySelector(".js-due-date-input");
   const dueDate = dateInputElement.value;
 
   // Check if name is empty
-  if (name.trim() !== '') {
+  if (name.trim() !== "") {
     const todo = {
       name: name,
-      dueDate: dueDate
+      dueDate: dueDate,
     };
     todoList.push(todo);
     console.log(todoList);
 
     // Clear the input fields after adding the todo
-    nameInputElement.value = '';
-    dateInputElement.value = '';
+    nameInputElement.value = "";
+    dateInputElement.value = "";
 
     renderTodoList();
   } else {
-    alert('Please enter a name for the todo.');
+    alert("Please enter a name for the todo.");
   }
-
 }
