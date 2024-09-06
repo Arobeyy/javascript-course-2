a38f430255051b9c7c46e83a76c9e54c2d2c8ddd
arobey <sumiyaakter.arobey@gmail.com>
2024-09-06 21:44:46 +0600

    event listener added

--- scripts/12-rock-paper-scissor-auto-play.js
+++ scripts/12-rock-paper-scissor-auto-play.js
@@ -35,6 +35,31 @@ function autoPlay() {
   }
 }
 
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('rock');
+  });
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('paper');
+  });
+
+document.querySelector('.js-scissor-button')
+  .addEventListener('click', () => {
+    playGame('scissor');
+  });
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper')
+  } else if (event.key === 's') {
+    playGame('scissor');
+  }
+});
+
 function playGame(playerMove) {
   computerMove = pickComputerMove();
 
